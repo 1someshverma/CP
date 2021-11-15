068e21dc3a783c7b415559a0372fff00fb01a306
Somesh <1someshverma@gmail.com>
2021-11-15 21:15:31 +0530

    A_Yellow_Cards.cpp

---
+++ codeforces/A_Yellow_Cards.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+using namespace std;
+#define ll long long
+#define gc getchar_unlocked
+#define ll long long
+#define PI 3.1415926535897932384626
+#define fo(i, n) for(i=0;i<n;i++)
+#define Fo(i, k, n) for(i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, x) for(auto it = x.begin(); it != x.end(); it++)
+#define trr(it, x) for(auto it = x.rbegin(); it != x.rend(); it+)
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+#define mod 1000000007
+void solve()
+{
+    int a1,a2,k1,k2,n;
+    cin>>a1>>a2>>k1>>k2>>n;
+    int x=0,y=0;
+    if(k1>k2)
+        {
+            swap(k1,k2);
+            swap(a1,a2);
+        }
+    int cnt=a1*(k1-1)+a2*(k2-1);
+    if(cnt<=0)
+        cout<<"0"<<" ";
+    else {
+        cout<<(n-cnt)<<" ";
+    }
+    if(n<=a1*k1)
+        cout<<n/k1<<endl;
+    else{
+        cout<<a1+(n-a1*k1)/k2<<endl;
+    }
+}
+int main() {
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+
+    ll test=1;
+//cin>>test;
+    while(test--)
+    {
+        solve();
+    }
+    return 0;
+}
