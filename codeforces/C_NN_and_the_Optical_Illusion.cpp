12c0438c4617737e0a86d2deac3e4ebb1b5ad6f5
1someshverma <1someshverma@gmail.com>
2022-01-23 11:28:46 +0530

    C_NN_and_the_Optical_Illusion.cpp

---
+++ codeforces/C_NN_and_the_Optical_Illusion.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define gc getchar_unlocked
+#define ll long long
+#define PI 3.1415926535897932384626
+#define fo(i, n) for(i=0;i<n;i++)
+#define Fo(i, k, n) for(i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, x) for(auto it = x.begin(); it != x.end(); it++)
+#define trr(it, x) for(auto it = x.rbegin(); it != x.rend(); it+)
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+#define mod 1000000007
+void solve()
+{
+    double n,r;
+    cin>>n>>r;
+    cout<<fixed<<setprecision(7)<<(r*(cos((PI*(n-2))/n/2))/(1-cos((PI*(n-2))/n/2)))<<endl;
+}
+int main() {
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+
+    ll test=1;
+//cin>>test;
+    while(test--)
+    {
+        solve();
+    }
+    return 0;
+}
