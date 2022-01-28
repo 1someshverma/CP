5cd55b97408463f3b8347cce39bf6fde72bf5958
1someshverma <1someshverma@gmail.com>
2022-01-28 21:35:03 +0530

    C_Tiles.cpp

---
+++ codeforces/C_Tiles.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+constexpr int MOD = 998244353;
+    
+int main() {
+    int R, C; cin >> R >> C;
+    int X = 1;
+    while (R--) X = (2*X)%MOD;
+    while (C--) X = (2*X)%MOD;
+    cout << X << endl;
+}
