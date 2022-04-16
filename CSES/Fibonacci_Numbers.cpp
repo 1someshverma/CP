2f23709259f310143e7de37c9ac2cfa28fa826fd
1someshverma <1someshverma@gmail.com>
2022-04-16 16:48:35 +0530

    Fibonacci_Numbers.cpp

---
+++ CSES/Fibonacci_Numbers.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+using namespace std;
+#define ll long long
+#define gc getchar_unlocked
+#define ll long long
+#define PI 3.1415926535897932384626
+#define fo(i, n) for(i=0;i<n;i++)
+#define Fo(i, k, n) for(i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, x) for(auto it = x.begin(); it != x.end(); it++)
+#define trr(it, x) for(auto it = x.rbegin(); it != x.rend(); it+)
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+const int MOD=1000000007;
+long long int inverse(long long int i){
+    if(i==1) return 1;
+    return (MOD - ((MOD/i)*inverse(MOD%i))%MOD+MOD)%MOD;
+}
+ll POW(ll a,ll b)
+{
+    if(b==0) return 1;
+    if(b==1) return a%MOD;
+    ll temp=POW(a,b/2);
+    if(b%2==0) return (temp*temp)%MOD;
+    else return (((temp*temp)%MOD)*a)%MOD;
+}
+map<ll,ll> f;
+
+ll fib(ll n)
+{
+    if(f.count(n)) return f[n];
+    if(n==0) return 0;
+    if(n==1 || n==2) return 1;
+    if(n%2==0)
+    {
+        ll k=n/2;
+        ll ret1=fib(k-1),ret2=fib(k);
+        return f[n]=((((2ll*ret1)%MOD+ret2)%MOD)*ret2)%MOD;
+    }
+    else
+    {
+        ll k=(n+1)/2;
+        ll ret1=fib(k-1),ret2=fib(k);
+        return f[n]=( (ret1*ret1)%MOD + (ret2*ret2)%MOD)%MOD;
+    }
+}
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll n;
+    cin>>n;
+    cout<<fib(n);
+}
