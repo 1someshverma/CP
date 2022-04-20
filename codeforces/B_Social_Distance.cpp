bf465134f0f46e8197b1edff0014a89a535fb5c9
1someshverma <1someshverma@gmail.com>
2022-04-20 10:15:50 +0530

    B_Social_Distance.cpp

---
+++ codeforces/B_Social_Distance.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+using namespace std;
+#define ll long long
+#define gc getchar_unlocked
+#define ll long long
+#define PI 3.1415926535897932384626
+#define fo(i, n) for(i=0;i<n;i++)
+#define Fo(i, k, n) for(i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, x) for(auto it = x.begin(); it != x.end(); it++)
+#define trr(it, x) for(auto it = x.rbegin(); it != x.rend(); it+)
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+#define mod 1000000007
+void solve()
+{
+        ll n,m;
+        cin >> n >> m;
+        vi a(n);
+
+        ll i;
+        fo(i,n)
+            cin >> a[i];
+        sort(all(a));
+
+        ll add=0;
+
+        for(i=1;i<n;i++)
+            add+=a[i];
+        add+=a[n-1];
+        add+=n;
+        // if yes or no
+        if(add<=m)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+}
+int main() {
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+
+    ll test=1;
+cin>>test;
+    while(test--)
+    {
+        solve();
+    }
+    return 0;
+}
